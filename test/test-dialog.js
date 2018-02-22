26fba0b310b7a273cb44b7279b51287f2315f269
Elliott Marquez <emarquez@google.com>
2018-02-22 15:09:16 -0800

    auto-converted by polymer-modulizer

--- test/test-dialog.js
+++ test/test-dialog.js
@@ -1,4 +1,7 @@
-<!--
+import { PaperDialogBehavior } from '../../paper-dialog-behavior/paper-dialog-behavior.js';
+import { Polymer } from '../../polymer/lib/legacy/polymer-fn.js';
+import { html } from '../../polymer/polymer.js';
+/**
 @license
 Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,20 +9,15 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-<link rel="import" href="../../paper-dialog-behavior/paper-dialog-behavior.html">
-
-<dom-module id="test-dialog">
-  <template>
+*/
+Polymer({
+  _template: html`
     <slot name="title"></slot>
     <paper-dialog-scrollable id="scrollable">
       <slot></slot>
     </paper-dialog-scrollable>
-  </template>
-  <script>
-    Polymer({
-      is: 'test-dialog',
-      behaviors: [Polymer.PaperDialogBehavior]
-    });
-  </script>
-</dom-module>
+`,
+
+  is: 'test-dialog',
+  behaviors: [PaperDialogBehavior]
+});
